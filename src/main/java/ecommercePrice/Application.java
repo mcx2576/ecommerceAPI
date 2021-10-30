f13690beb865ac8246c25b985f8865bfbe5522fd
mcx2576 <mcx2576@gmail.com>
2021-10-31 01:58:48 +0200

    Spring application

--- src/main/java/ecommercePrice/Application.java
+++ src/main/java/ecommercePrice/Application.java
@@ -1,12 +1,12 @@
-package ecommercePrice;
+package EcommercePrice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Configuration
-@ComponentScan
+@RestController
 @EnableAutoConfiguration
 public class Application {
 
