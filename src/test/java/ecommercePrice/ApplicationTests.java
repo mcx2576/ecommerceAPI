2a925cddf92c4d4d8db72eec71387a74ea489f39
mcx2576 <mcx2576@gmail.com>
2021-10-31 01:55:20 +0200

    Adding integration tests

--- src/test/java/ecommercePrice/ApplicationTests.java
+++ src/test/java/ecommercePrice/ApplicationTests.java
@@ -1,6 +1,7 @@
 package ecommercePrice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(classes = Application.class)
 class ApplicationTests {
